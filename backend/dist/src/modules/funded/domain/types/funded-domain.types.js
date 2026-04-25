c235c6e8540bdebb6e0b194d54b1092baf3bc3e5
KingsonCode <kingsontraders@gmail.com>
2026-04-26 01:00:45 +0300

    Ignore backend build output

--- backend/dist/src/modules/funded/domain/types/funded-domain.types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=funded-domain.types.js.map
--- backend/dist/src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const funded_routes_1 = require("./modules/funded/funded.routes");
-const app = (0, express_1.default)();
-app.use(express_1.default.json());
-app.use("/api/funded", funded_routes_1.fundedRouter);
-//# sourceMappingURL=server.js.map
