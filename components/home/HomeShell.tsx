85bc0162021b7711d129f174f969b9eaabf627c7
KingsonCode <kingsontraders@gmail.com>
2026-05-14 23:46:17 +0200

    refactor(home): remove noisy market and news sections

--- components/home/HomeShell.tsx
+++ components/home/HomeShell.tsx
@@ -1,11 +1,7 @@
 import Hero from "@/components/Hero";
 import WhyVelmenora from "@/components/WhyVelmenora";
-import LearnForex from "@/components/LearnForex";
 import FinalCTA from "@/components/FinalCTA";
 import TopBrokers from "@/components/TopBrokers";
-import MarketHeroChart from "@/components/MarketHeroChart";
-import TopMarkets from "@/components/TopMarkets";
-import HomeCalendar from "@/components/home/HomeCalendar";
 
 export type HomeLang = "en" | "ar" | "de" | "fr";
 
@@ -80,21 +76,11 @@ export default function HomeShell({ lang }: Props) {
                 </div>
             </section>
 
-            <MarketHeroChart lang={lang} />
-
-            <section className="border-y border-white/10">
-                <TopMarkets lang={lang} />
-            </section>
-
-            <section className="mx-auto max-w-7xl px-4 py-12 lg:px-6">
-                <HomeCalendar />
-            </section>
 
             <TopBrokers />
 
             <WhyVelmenora lang={lang} />
 
-            <LearnForex lang={lang} />
 
             <FinalCTA lang={lang} />
         </main>
